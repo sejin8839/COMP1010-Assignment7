4db2485493a172a777cdc586f484473dffb0132e
sejin8839 <sejinon01@gmail.com>
2023-05-15 00:03:35 +0900

    Update Problem2.py

--- Problem2.py
+++ Problem2.py
@@ -1,3 +1,7 @@
+# u1311019
+# Sejin Yoon
+
+
 class Library:
     def __init__(self, acc_number, title, author):
         self.acc_number = acc_number
@@ -12,3 +16,12 @@ class Library:
     def compute(self, days_late):
         fine = days_late * 1.50
         print(f"Fine charged: ${fine:.2f}")
+
+# create a Library object with acc_number=123, title="Book Title", and author="John Doe"
+book = Library(123, "Book Title", "John Doe")
+
+# call the read method to display the book's information
+book.read()
+
+# call the compute method to calculate and display the fine for 3 days late
+book.compute(3)
